ec14d23e3f81c9a523a445dae96f37f46b3fd27a
amodelollamas00 <amodelollamas00@gmail.com>
2021-10-15 11:39:42 +0200

    Ejercicio de la matriz terminado

--- Tema 4/Ejercicio_4.php
+++ Tema 4/Ejercicio_4.php
@@ -1,4 +1,34 @@
 <?php
 //Crea una matriz de 10 x 10 y calcula la suma de la diagonal principal y la suma de la diagonal secundaria y
 //muestre por pantalla la diferencia de ambas
+
+    $Matriz;
+    $Mayor = 0;
+    $Menor = 0;
+
+    for($k=0; $k<10; $k++){
+        for($j=0; $j<10; $j++){
+            $Matriz[$k][$j]=rand(0,9);
+            if($k==$j){
+                $Mayor += $Matriz[$k][$j];
+            }
+
+            if($k+$j == sizeof($Matriz)){
+                $Menor += $Matriz[$k][$j];
+            }
+        }
+    }
+
+    echo "Imprimimos nuestra matriz <br><br>";
+
+    for($k=0; $k<10; $k++){
+        echo "(";
+        for($j=0; $j<10; $j++){
+            echo $Matriz[$k][$j]." ";
+        }
+        echo ") <br> ";
+    }
+
+    echo "<br> La suma de la diagonal mayor da $Mayor <br>";
+    echo "La suma de la diagonal mayor da $Menor";
 ?>
